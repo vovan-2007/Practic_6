59fb2595b8be9ecdcd6926ede9c02be7775c4096
amogus <110690851+vovan-2007@users.noreply.github.com>
2025-06-04 12:19:56 +0400

    Add files via upload

---
+++ lab6t19/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace lab6t19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = { 1, 2, 3, 4, 5 };
+            int[] array2 = { 3, 4, 5, 6, 7 };
+            int[] OneNoTwo = array1.Except(array2).ToArray();
+            Console.WriteLine("19.1. Элементы в первом, но не во втором: " + string.Join(", ", OneNoTwo));
+            int[] obshac = array1.Intersect(array2).ToArray();
+            Console.WriteLine("19.2. Общие элементы: " + string.Join(", ", obshac));
+            int[] NeObshac = array1.Union(array2).Except(obshac).ToArray();
+            Console.WriteLine("19.3. Объединение без общих элементов: " + string.Join(", ", NeObshac));
+            Console.WriteLine("Нажмите любую клавишу для выхода...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lab6t21/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace lab6t21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите размер массива n: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] array = new int[n];
+            Random rnd = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = rnd.Next(1, 100);
+            }
+            Console.WriteLine("Исходный массив: " + string.Join(", ", array));
+            Console.Write("Введите количество элементов для сдвига (m): ");
+            int m = int.Parse(Console.ReadLine());
+            m = m % n;
+            int[] sdvgArray = array.Skip(m).Concat(array.Take(m)).ToArray();
+            Console.WriteLine($"Массив после сдвига на {m} элементов: " + string.Join(", ", sdvgArray));
+            Console.WriteLine("Нажмите любую клавишу для выхода...");
+            Console.ReadKey();
+        }
+    }
+}
