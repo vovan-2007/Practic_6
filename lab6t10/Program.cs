9f8ce3a2e4298a0d4ba9950cdfa17e782a5bfab6
amogus <110690851+vovan-2007@users.noreply.github.com>
2025-06-04 12:17:50 +0400

    Add files via upload

---
+++ lab6t10/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace lab6t10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите размер массива n: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] array = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Введите элемент {i + 1}: ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            bool hasNegative = array.Any(x => x < 0);
+            Console.WriteLine("Есть отрицательные элементы: " + hasNegative);
+            Console.WriteLine("Нажмите любую клавишу для выхода...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lab6t12/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace lab6t12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите размер массива n: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] array = new int[n];
+            Random rnd = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = rnd.Next(1, 100);
+            }
+            Console.WriteLine("Исходный массив: " + string.Join(", ", array));
+            int[] newArray = array.Select(x => x % 2 == 0 ? x : x + 1).ToArray();
+            Console.WriteLine("Массив с четными элементами: " + string.Join(", ", newArray));
+            Console.WriteLine("Нажмите любую клавишу для выхода...");
+            Console.ReadKey();
+        }
+    }
+}
