adbf4cbdc69c71c0b9469568f86b70bdec33348d
amogus <110690851+vovan-2007@users.noreply.github.com>
2025-06-04 12:20:20 +0400

    Add files via upload

---
+++ lab6t22/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace lab6t22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static void BubbleSort(int[] arr)
+            {
+                for (int i = 0; i < arr.Length - 1; i++)
+                {
+                    for (int j = 0; j < arr.Length - i - 1; j++)
+                    {
+                        if (arr[j] > arr[j + 1])
+                        {
+                            (arr[j], arr[j + 1]) = (arr[j + 1], arr[j]);
+                        }
+                    }
+                }
+            }
+            static void SelectionSort(int[] arr)
+            {
+                for (int i = 0; i < arr.Length - 1; i++)
+                {
+                    int minIndex = i;
+                    for (int j = i + 1; j < arr.Length; j++)
+                    {
+                        if (arr[j] < arr[minIndex])
+                        {
+                            minIndex = j;
+                        }
+                    }
+                    (arr[i], arr[minIndex]) = (arr[minIndex], arr[i]);
+                }
+            }
+            static void QuickSort(int[] arr, int left, int right)
+            {
+                if (left < right)
+                {
+                    int pivot = Part(arr, left, right);
+                    QuickSort(arr, left, pivot - 1);
+                    QuickSort(arr, pivot + 1, right);
+                }
+            }
+            static int Part(int[] arr, int left, int right)
+            {
+                int pivot = arr[right];
+                int i = left - 1;
+
+                for (int j = left; j < right; j++)
+                {
+                    if (arr[j] < pivot)
+                    {
+                        i++;
+                        (arr[i], arr[j]) = (arr[j], arr[i]);
+                    }
+                }
+                (arr[i + 1], arr[right]) = (arr[right], arr[i + 1]);
+                return i + 1;
+            }
+            static void Main(string[] args)
+            {
+                Console.Write("Введите размер массива n: ");
+                int n = int.Parse(Console.ReadLine());
+                int[] array = new int[n];
+                Random rnd = new Random();
+                for (int i = 0; i < n; i++)
+                {
+                    array[i] = rnd.Next(1, 100);
+                }
+                Console.WriteLine("Исходный массив: " + string.Join(", ", array));
+                int[] bubbleSorted = (int[])array.Clone();
+                BubbleSort(bubbleSorted);
+                Console.WriteLine("22.1. Сортировка пузырьком: " + string.Join(", ", bubbleSorted));
+                int[] selectionSorted = (int[])array.Clone();
+                SelectionSort(selectionSorted);
+                Console.WriteLine("22.2. Сортировка выбором: " + string.Join(", ", selectionSorted));
+                int[] quickSorted = (int[])array.Clone();
+                QuickSort(quickSorted, 0, quickSorted.Length - 1);
+                Console.WriteLine("22.3. Быстрая сортировка: " + string.Join(", ", quickSorted));
+            }
+    }
+}
---
+++ lab6t23/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace lab6t23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static int DigitSum(int num)
+            {
+                int sum = 0;
+                while (num != 0)
+                {
+                    sum += num % 10;
+                    num /= 10;
+                }
+                return sum;
+            }
+            static int FirstDigit(int num)
+            {
+                while (num >= 10) num /= 10;
+                return num;
+            }
+            static void Main(string[] args)
+            {
+                Console.Write("Введите размер массива n: ");
+                int n = int.Parse(Console.ReadLine());
+                int[] array = new int[n];
+                Random rnd = new Random();
+                for (int i = 0; i < n; i++)
+                {
+                    array[i] = rnd.Next(1, 100);
+                }
+                Console.WriteLine("Исходный массив: " + string.Join(", ", array));
+                int[] sumSorted = array.OrderBy(x => DigitSum(x)).ToArray();
+                Console.WriteLine("Отсортировано по сумме цифр: " + string.Join(", ", sumSorted));
+                int[] lastDigitSorted = array.OrderBy(x => x % 10).ToArray();
+                Console.WriteLine("Отсортировано по последней цифре: " + string.Join(", ", lastDigitSorted));
+                int[] firstDigitSorted = array.OrderBy(x => FirstDigit(x)).ToArray();
+                Console.WriteLine("Отсортировано по первой цифре: " + string.Join(", ", firstDigitSorted));
+                Console.WriteLine("Нажмите любую клавишу для выхода...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
